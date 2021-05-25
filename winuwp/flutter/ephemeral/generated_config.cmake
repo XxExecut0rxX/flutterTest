c287dde5e41c5b8ab897480239e757b2002c6681
XxExecut0rxX <82061636+XxExecut0rxX@users.noreply.github.com>
2021-05-25 18:11:27 -0400

    This is my first commit with flutter

---
+++ winuwp/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Google Drive\\Flutte projects\\test\\testproject" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter"
+  "PROJECT_DIR=D:\\Google Drive\\Flutte projects\\test\\testproject"
+  "FLUTTER_ROOT=D:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Google Drive\\Flutte projects\\test\\testproject\\winuwp\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Google Drive\\Flutte projects\\test\\testproject"
+  "FLUTTER_TARGET=D:\\Google Drive\\Flutte projects\\test\\testproject\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Google Drive\\Flutte projects\\test\\testproject\\.dart_tool\\package_config.json"
+)
